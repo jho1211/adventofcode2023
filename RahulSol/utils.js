82800402e23ced0a73cac2799e27cb0d97f65cf7
rramjuttun <47162900+rramjuttun@users.noreply.github.com>
2023-12-03 21:54:38 -0800

    Update utils.js

--- RahulSol/utils.js
+++ RahulSol/utils.js
@@ -3,16 +3,16 @@
 function main(p1, p2) {
     switch(process.argv[2]) {
         case "1":
-            p1();
+            console.log(p1());
             break;
 
         case "2":
-            p2();
+            console.log(p2());
             break;
 
         default:
-            p1();
-            p2();
+            console.log(p1());
+            console.log(p2());
             break;
     }
 }
